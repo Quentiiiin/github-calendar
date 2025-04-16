9c4e00e31e585d675f17d085c6b5393ccf86fbe0
Quentin <der.hookipianer@web.de>
2025-04-16 13:05:37 +0200

    refreshing data ever 60s

--- src/app/user/[slug]/page.tsx
+++ src/app/user/[slug]/page.tsx
@@ -1,15 +1,63 @@
+'use client'
 import { ActivityCalendar } from 'react-activity-calendar';
+import { useState, useEffect } from 'react';
 
-export default async function Page({
+export default function Page({
     params,
 }: {
-    params: Promise<{ slug: string }>
+    params: { slug: string }
 }) {
-    const slug = (await params).slug;
-    const currentYear = new Date().getFullYear();
+    const [slug, setSlug] = useState<string | null>(null);
+    const [data, setData] = useState<any[]>([]);
+
+    useEffect(() => {
+        params.then((resolvedParams) => {
+            setSlug(resolvedParams.slug);
+        });
+    }, [params]);
+
+    useEffect(() => {
+        let intervalId: NodeJS.Timeout | null = null;
+
+        const fetchDataAndUpdate = async (currentSlug: string) => {
+            const newData = await fetchData(currentSlug);
+            setData(newData);
+        };
+
+        if (slug) {
+            fetchDataAndUpdate(slug);
+            intervalId = setInterval(() => {
+                if (slug) {
+                    fetchDataAndUpdate(slug);
+                }
+            }, 1000 * 60);
+        }
+
+        return () => {
+            if (intervalId) {
+                clearInterval(intervalId);
+            }
+        };
+    }, [slug]);
+
+    return (
+        <div className='flex w-screen h-screen justify-center items-center overflow-hidden page-container'>
+            <div className='overflow-hidden scale-150'>
+                {data && data.length > 0 ? (
+                    <ActivityCalendar data={data} colorScheme={'dark'} />
+                ) : (
+                    <p>No contribution data found for the current year.</p>
+                )}
+            </div>
+        </div>
+    );
+}
+
+async function fetchData(slug: string): Promise<any[]> {
     const url = `https://github-contributions-api.jogruber.de/v4/${slug}`;
     const res = await fetch(url);
     const json = await res.json();
+    const currentYear = new Date().getFullYear();
     const rawData = json.contributions;
     const data: any[] = [];
     rawData.forEach((entry: any) => {
@@ -18,12 +66,5 @@ export default async function Page({
             data.push(entry);
         }
     });
-
-    return (
-        <div className='flex w-screen h-screen justify-center items-center overflow-hidden page-container'>
-            <div className='overflow-hidden scale-150'>
-                <ActivityCalendar data={data} colorScheme={'dark'} />
-            </div>
-        </div>
-    );
+    return data;
 }
